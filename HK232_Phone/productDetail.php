1b120592671b8c846b9a97841a73bcf0c76d8be0
Ngô Quang Hải <80143180+NgoQuangHai19@users.noreply.github.com>
2024-04-30 13:48:54 +0700

    revise productpage

--- HK232_Phone/productDetail.php
+++ HK232_Phone/productDetail.php
@@ -62,7 +62,6 @@ if ($result_rating_avg && $result_rating_avg->num_rows > 0) {
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 </head>
-
 <body>
   <div class="app">
     <?php
